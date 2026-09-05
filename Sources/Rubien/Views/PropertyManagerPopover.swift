e978df15e1719ad34198806d6f9aca3456b172ac
devzhk <hzzheng@caltech.edu>
2026-09-04 23:52:58 -0700

    style: set column manager width to 200 points

--- Sources/Rubien/Views/PropertyManagerPopover.swift
+++ Sources/Rubien/Views/PropertyManagerPopover.swift
@@ -140,7 +140,7 @@ struct PropertyManagerPopover: View {
                 .padding(12)
             }
         }
-        .frame(width: 220)
+        .frame(width: 200)
         .activatePopoverHover()
     }
 }
