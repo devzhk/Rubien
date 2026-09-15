767326a1e6589730ec48b6312c793e471a3fcc5d
devzhk <hzzheng@caltech.edu>
2026-09-15 00:05:03 -0700

    chore: advance 0.7.9 candidate to build 49

--- Sources/RubienCLI/GeneratedVersion.swift
+++ Sources/RubienCLI/GeneratedVersion.swift
@@ -2,5 +2,5 @@
 // Do not edit by hand. CI fails if this drifts from those files.
 enum RubienCLIVersion {
     static let marketing = "0.7.9"
-    static let build = 48
+    static let build = 49
 }
